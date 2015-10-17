abfc8d085506f822d30774261306521dd0cc64a3
Eliott Hauteclair <eliott@MacBook-van-Eliott.local>
2015-10-17 13:26:21 +0200

    updates v1.2.2

--- PONKapp/plugins/chromecast/chromecast.js
+++
@@ -1,9 +0,0 @@
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Chromecast", "echo", [str]);
-};
-
-window.listDevices = function(callback) {
-  cordova.exec(callback, function(err) { callback('Cannot enumerate devices'); }, "Chromecast", "listDevices", []);
-}
